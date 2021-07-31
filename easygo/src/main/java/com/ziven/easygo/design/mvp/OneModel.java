2dd032c570e036ca5789da082e97e2aee600bd5e
ziven2018 <zhouzhiyuan0212@163.com>
2021-07-31 23:41:28 +0800

    OneModel新增参数获取方法

--- easygo/src/main/java/com/ziven/easygo/design/mvp/OneModel.java
+++ easygo/src/main/java/com/ziven/easygo/design/mvp/OneModel.java
@@ -30,7 +30,42 @@ public abstract class OneModel extends AbstractModel<IOnePresenter> {
         return params == null ? null : params.get(key);
     }
 
+    protected Object getParam(@NonNull final Object key, @Nullable final Map<Object, Object> params, @NonNull Object def) {
+        return params == null ? def : params.get(key);
+    }
+
     protected <T> T getTargetParam(@NonNull final Object key, @Nullable final Map<Object, Object> params) {
         return EasyUtils.transition(getParam(key, params));
+    }
+
+    protected <T> T getTargetParam(@NonNull final Object key, @Nullable final Map<Object, Object> params, @NonNull Object def) {
+        return EasyUtils.transition(getParam(key, params, def));
+    }
+
+    protected int getInt(@NonNull final Object key, @Nullable final Map<Object, Object> params) {
+        return getInt(key, params, 0);
+    }
+
+    protected int getInt(@NonNull final Object key, @Nullable final Map<Object, Object> params, int def) {
+        Object target = getParam(key, params);
+        return target instanceof Integer ? EasyUtils.transition(target) : def;
+    }
+
+    protected boolean getBoolean(@NonNull final Object key, @Nullable final Map<Object, Object> params) {
+        return getBoolean(key, params, false);
+    }
+
+    protected boolean getBoolean(@NonNull final Object key, @Nullable final Map<Object, Object> params, boolean def) {
+        Object target = getParam(key, params);
+        return target instanceof Boolean ? EasyUtils.transition(target) : def;
+    }
+
+    protected String getString(@NonNull final Object key, @Nullable final Map<Object, Object> params) {
+        return getString(key, params, EasyUtils.EMPTY);
+    }
+
+    protected String getString(@NonNull final Object key, @Nullable final Map<Object, Object> params, String def) {
+        Object target = getParam(key, params);
+        return target instanceof String ? EasyUtils.transition(target) : def;
     }
 }
